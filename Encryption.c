7794367efeff1c9d2eb817392ae95d3d4e23346b
deepakasd <54941221+deepakasd@users.noreply.github.com>
2020-02-05 06:59:19 +0530

    Create Encryption.c

---
+++ Encryption.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char inputfile[20],outputfile[20],ch;
+	int numAscii;
+	FILE *fd1,*fd2;
+	printf("\n enter the name of file to be encrypted\n");
+	scanf("%s",inputfile);
+	printf("enter the name of the output file:\n");
+	scanf("%s",outputfile);
+	fd1=fopen(inputfile,"r");
+	if(fd1==NULL){
+		printf("input file doesnot exists\n");
+		exit(0);
+	}else{
+		fd2=fopen(outputfile,"w");
+		ch=fgetc(fd1);
+		while(ch!=EOF){
+			numAscii=(int)ch;
+			numAscii=numAscii+5;
+			fprintf(fd2,"%c",numAscii);
+			ch=fgetc(fd1);
+		}
+		fclose(fd2);
+	}
+	fclose(fd1);
+	printf("\n File is encrypted with name %s",outputfile);
+	return 0;
+}
+
